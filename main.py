144842f15f86e0130c2aeef11b420ba326d76d59
user-dam <dam67@dam67>
2025-03-26 19:07:15 +0100

    Modificar puerto

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main(page: ft.Page):
     page.add(ft.Text("¡Hola, Jordi!"))
 
 if __name__ == '__main__':
-    ft.app(target=main,view=ft.WEB_BROWSER, port=8080)
+    ft.app(target=main,view=ft.WEB_BROWSER, port=30047)
 
 
 
